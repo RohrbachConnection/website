c131a2bb27f32804ecc21f2c714277cc7a1f77f4
hermanrohrbach <hermanrohrbach@gmail.com>
2017-09-26 19:16:43 +0200

    Read shows from XML
    This change ensures that the shows are read from the xml file in the xml folder, so that shows can be added there rather than directly in the html file.

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,8 @@ var rcMusic = "html/rcMusic.html";
 var rcShows = "html/rcShows.html";
 var rcPhotos = "html/rcPhotos.html";
 
+var showsXML = "xml/shows.xml";
+
 var currentTab = "";
 
 //Get snippet of html for dynamic content
@@ -76,6 +78,64 @@ function loadShows() {
 
 function loadShowsHelper() {
     setSelectedTab("showsTab");
+    loadShowsXML();
+}
+
+//Load shows xml
+function loadShowsXML() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            readShowsXML(this);
+        }
+    };
+    xhttp.open("GET", showsXML, true);
+    xhttp.send();
+}
+
+function readShowsXML(xml) {
+    shows = xml.responseXML.getElementsByTagName("show");
+    var row = $("<tr></tr>");
+    var column = $("<td></td>");
+    var tag;
+    for (var i = 0; i < shows.length; i++) {
+        var date = new Date(selectTag(shows[i], "date").innerHTML);
+        column.append(date.toLocaleDateString());
+        row.append(column);
+
+        column = $("<td></td>");
+        tag = selectTag(shows[i], "venue");
+        addVenueToRow(row, column, tag);
+
+        column = $("<td></td>");
+        column.append(selectTag(shows[i], "city").innerHTML + ", " +
+            selectTag(shows[i], "country").innerHTML);
+        row.append(column);
+
+        $("#showsTable").append(row);
+
+        row = $("<tr></tr>");
+        column = $("<td></td>");
+    }
+}
+
+//Selects the first tag with the passed name from the passed tag
+function selectTag(tag, name) {
+    return tag.getElementsByTagName(name)[0];
+}
+
+//Checks if the link is non-empty and adds it to the column
+function addVenueToRow(row, column, tag) {
+    var link = $(tag).attr("link");
+    if (typeof link != "undefined" && link != "") {
+        a = $("<a></a>").attr("href", link);
+        a.append(tag.innerHTML);
+        column.append(a);        
+    }
+    else {
+        column.append(tag.innerHTML);
+    }
+    row.append(column);
 }
 
 //Load photos page
